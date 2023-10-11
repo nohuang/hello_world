89bd65862dba558230411381ea8257b965b11234
nohuang <144878921+nohuang@users.noreply.github.com>
2023-10-11 23:21:22 +0800

    h

--- w5.R
+++ w5.R
@@ -7,13 +7,12 @@ flights = jsonlite::fromJSON("international_flights.json")
 View(flights)
 
 # data information ----
-data1 = list(
+data1 <- list(
   file = "international_flights.json",
-  name = "國際航空定期時刻表",
-  source_link = "https://data.gov.tw/dataset/161167"
-  )
-
-data1$file
+  meta = list(
+    name = "國際航空定期時刻表",
+    source_link = "https://data.gov.tw/dataset/161167")
+)
 
 flights <- list(
   data = list(data1)
@@ -26,6 +25,11 @@ filepath = flights$data[[1]]$file
 flightData = jsonlite::fromJSON(filepath)
 
 flights$data[[1]]$data_frame <- flightsData
+flightsData
 
-saveRDS(flights, file="flights.rds")
+typeof(flightsData)
+class(flightsData)
+
+saveRDS(flights, "flights.rds")
+
 
