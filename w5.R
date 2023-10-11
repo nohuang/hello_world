5c372e32992cc282942f8d5ed92f5ebef6a3618f
nohuang <144878921+nohuang@users.noreply.github.com>
2023-10-11 15:01:16 +0800

    Add files via upload

---
+++ w5.R
@@ -0,0 +1,29 @@
+# my economic grades
+MyEcnomicGrades = c(90,80)
+my_ecnomics_grades = c(90,80)
+
+# import json file -----
+flights = jsonlite::fromJSON("international_flights.json")
+View(flights)
+
+# data information ----
+data1 = list(
+  file = "international_flights.json",
+  name = "國際航空定期時刻表",
+  source_link = "https://data.gov.tw/dataset/161167"
+  )
+
+data1$file
+
+flights <- list(
+  data = list(data1)
+)
+filepath = flights$data[[1]]$file
+
+flightData = jsonlite::fromJSON(filepath)
+
+filepath = flights$data_frame <- flightsData
+
+filepath = flights$data_frame 
+
+saveRDS(flights, file="flights.rds")
