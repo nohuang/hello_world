d129e7c687487d81266e8cec8f7d03ac5124da1f
nohuang <144878921+nohuang@users.noreply.github.com>
2023-12-06 14:52:33 +0800

    Add files via upload

---
+++ final project.R
@@ -0,0 +1,3 @@
+library(readr)
+Download <- read_csv("https://ws.hl.gov.tw/Download.ashx?u=LzAwMS9VcGxvYWQvNTE4L3JlbGZpbGUvMjI2NDYvMTQ4NjcxLzYwNTM0MWJlLWNkNjktNGQ2My05ZDFjLTg4MzIxZDU5NTU1ZS5jc3Y%3d&n=MTEyMTAtLTU2Mi1uZXcuY3N2")
+View(Download)
